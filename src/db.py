082a001b62d077b516356a1e7001a40744cf35a5
yoshinao.komatsu <yoshinao.komatsu@visasq.com>
2024-01-10 12:20:15 +0900

    wip

--- src/db.py
+++ src/db.py
@@ -7,17 +7,20 @@ ref: https://docs.sqlalchemy.org/en/20/orm/session_basics.html#using-a-sessionma
 ref: https://docs.sqlalchemy.org/en/20/orm/session_basics.html#when-do-i-construct-a-session-when-do-i-commit-it-and-when-do-i-close-it
 """  # noqa
 
+import os
+
 from sqlalchemy import URL, create_engine, text
 from sqlalchemy.orm import sessionmaker
 
 url = URL.create(
     drivername="mysql+pymysql",
-    username="testuser",
-    password="testpassword",
-    host="localhost",
-    database="testdb",
-    port=3306,
+    username=os.getenv("DB_USERNAME", "testuser"),
+    password=os.getenv("DB_PASSWORD", "testpassword"),
+    host=os.getenv("DB_HOST", "localhost"),
+    database=os.getenv("DB_DATABASE", "testdb"),
+    port=int(os.getenv("DB_PORT", "3306")),
 )
+print(f"### db conn url[{url}]")
 engine = create_engine(url, echo=True)
 Session = sessionmaker(bind=engine)
 
